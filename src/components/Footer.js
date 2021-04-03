311878ec30c00bd891e76be03f0f083fee8af9e4
Hyunji Kim <creative.hkim@gmail.com>
2021-04-03 15:33:26 -0400

    react build script executed, rearranged components and container files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <small style={{ fontWeight:'bold', color: '#81317d', position: 'fixed', bottom: '30px', left: '50%', transform: 'translateX(-50%)'}}>Built by ©Hyunji Kim 2021</small>
+    </div>
+  )
+}
+
+export default Footer
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import Footer from '../components/Footer';
 
 import './App.css';
 
@@ -32,22 +34,25 @@ componentDidMount() {
   
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if(this.state.robots.length === 0) {
-      return <h1>Loading...</h1>
-    } else {
-    return (
+      return !robots.length ? 
+      <h1>Loading...</h1> :
+      (
         <div className='tc'>
           <h1 className='f1' >RoboFriends</h1>
           <SearchBox searchChange={ this.onSearchChange }/>
-          <CardList robots={ filteredRobots } />
+          <Scroll>
+            <CardList robots={ filteredRobots } />
+          </Scroll>
+          <Footer />
         </div>
       );
     }
   }
   
-}
+
 
 export default App;
