873df725d0f3a33afe2989d0eddb1e3009debd59
Hyunji Kim <creative.hkim@gmail.com>
2021-04-16 19:49:52 -0400

    adding files

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id }) => {
   return (
     
     <div className='tc bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id}?200x200?set=set4?bgset=bg1`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
